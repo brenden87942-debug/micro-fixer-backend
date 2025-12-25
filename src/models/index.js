44b60512d12de59409245794be142b849042c0fe
Brenden Anglin <you@example.com>
2025-12-24 23:12:02 -0700

    Fix Sequelize init + add health route

--- src/models/index.js
+++ src/models/index.js
@@ -1,38 +1,39 @@
-'use strict';
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const configAll = require("../../config/config.json");
+const config = configAll[env] || configAll.development;
 
 const db = {};
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  // If config.url exists, use it. Otherwise (sqlite), use dialect+storage.
-  if (config.url) {
-    sequelize = new Sequelize(config.url, config);
-  } else {
-    sequelize = new Sequelize({
-      dialect: config.dialect,
-      storage: config.storage
-    });
-  }
-}
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+
+// Create sequelize instance from config
+const sequelize = new Sequelize({
+  dialect: config.dialect,
+  storage: config.storage,
+  logging: config.logging ?? false,
+});
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 &&
+      file !== basename &&
+      file.slice(-3) === ".js"
+    );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const modelFactory = require(path.join(__dirname, file));
+    const model = modelFactory(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
+Object.keys(db).forEach((modelName) => {
+  if (typeof db[modelName].associate === "function") {
     db[modelName].associate(db);
   }
 });
